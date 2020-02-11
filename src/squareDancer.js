042535f192868c34b57a27e9a0c84273101db432
jscholar <jinruncai@gmail.com>
2020-02-10 20:16:20 -0800

    Add a squareDancer subclass of Dancer
    Add button in dancefloor.html to add squareDancer.

---
+++ src/squareDancer.js
@@ -0,0 +1,44 @@
+var squareDancer = function (top, left, timeBetweenSteps) {
+  randomColoredDancer.call(this, top, left, timeBetweenSteps);
+  this.direction = 'right';
+  this.$node.css({
+    transition: timeBetweenSteps + 'ms',
+    'transition-timing-function': 'linear'
+  });
+};
+
+squareDancer.prototype = Object.create(randomColoredDancer.prototype);
+
+squareDancer.prototype.constructor = squareDancer;
+
+squareDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+
+  // Check which direction
+  //   Go that direction and then change directions
+
+  var distance = $("body").height() / 8; // Move down
+  // Dance like a square
+
+  switch (this.direction) {
+  case 'down':
+    this.top += distance;
+    this.direction = 'right';
+    break;
+  case 'right':
+    this.left += distance;
+    this.direction = 'up';
+    break;
+  case 'up':
+    this.top -= distance;
+    this.direction = 'left';
+    break;
+  case 'left':
+    this.left -= distance;
+    this.direction = 'down';
+    break;
+  }
+
+  this.setPosition();
+
+};
