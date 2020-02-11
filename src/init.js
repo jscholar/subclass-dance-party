7c2690de8cedf92d9f577bb29868695626f4ca86
jscholar <jinruncai@gmail.com>
2020-02-10 17:41:24 -0800

    added line up dancers function

--- src/init.js
+++ src/init.js
@@ -22,12 +22,21 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+    window.dancers.push(dancer);
+
     $('body').append(dancer.$node);
+  });
+
+  $('.lineUpDancersButton').on('click', function(event) {
+    for (var i = 0; i < window.dancers.length; i++) {
+      window.dancers[i].lineUp(i, window.dancers.length);
+    }
   });
 });
 
