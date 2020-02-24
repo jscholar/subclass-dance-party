113ced16d18c7cba58dc97606ed73173adddfe8e
Jin Cai <jinruncai@gmail.com>
2020-02-23 23:00:13 -0800

    Improve accuracy of world

--- src/world/World.js
+++ src/world/World.js
@@ -1,8 +1,8 @@
 var World = {
   scale: 0.01,
-  gravity: 10,
+  gravity: 9.81,
   dragWater: (
-    1 / 2
+    1 / 2 // For 1/2 v^2
     * 1 // (Drag coefficient of shape) Person is approx. 1.0
     * 1 // Density of the fluid
   ),
