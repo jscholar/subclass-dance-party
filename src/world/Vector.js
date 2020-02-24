d6f6dcd93fcd2036af2d1ac8e2494c1628a07385
Jin Cai <jinruncai@gmail.com>
2020-02-23 23:01:09 -0800

    Fix bug for normalizing 0 vectors. Add subtract method.

--- src/world/Vector.js
+++ src/world/Vector.js
@@ -6,19 +6,31 @@ var Vector = function (x, y) {
 Vector.prototype.add = function (vector) {
   this.x += vector.x;
   this.y += vector.y;
+  return this;
+};
+
+Vector.prototype.subtract = function (vector) {
+  this.x -= vector.x;
+  this.y -= vector.y;
+  return this;
 };
 
 Vector.prototype.scale = function (scalar) {
   this.x = scalar * this.x;
   this.y = scalar * this.y;
+  return this;
 };
 
 /**
  * Return a normalized Vector object
  */
 Vector.prototype.normalized = function() {
+  let magnitude = this.getMagnitude();
+  if (magnitude === 0) {
+    return new Vector(0, 0);
+  }
   return new Vector(
-    this.x / this.getMagnitude(), this.y / this.getMagnitude()
+    this.x / magnitude, this.y / magnitude
   );
 };
 
